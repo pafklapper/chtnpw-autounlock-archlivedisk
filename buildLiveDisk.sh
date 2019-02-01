0819d79d5f7ecb5f6f9b9819580bdd63951c0aed
PafKlapper <stan@wopwop.cc>
2019-02-02 00:12:20 +0100

    various

--- buildLiveDisk.sh
+++ buildLiveDisk.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 # run this script to build livedisk
 
-livediskWD=`mktemp -d`
+# GLOBVARS
+wd="$(dirname $0)"
+livediskWD="$wd/wd"
+livediskOUT="$wd/iso"
 
+# trap
 finish() {
 rm -rf $livediskWD
 }
 trap finish EXIT SIGINT SIGTERM
 
+logp beginsection
+logp info "Generating ISO, output @ $livediskOUT"
 
-pacman -Qi archiso || pacman -Sy archiso
-pacman -Qi arch-install-scripts || pacman -Sy arch-install-scripts 
+pacman -Qi archiso || logp info "Installing package 'archiso'..." && pacman -Sy archiso
+pacman -Qi arch-install-scripts || logp info "Installing package 'arch-install-scripts'..." && pacman -Sy arch-install-scripts 
 
-cp $(dirname $0)/windowsAutoAdminUnlock.service $(dirname $0)/livedisk/airootfs/etc/systemd/system/
-cp $(dirname $0)/windowsAutoAdminUnlock.sh $(dirname $0)/livedisk/airootfs/root/
+cp $wd/windowsAutoAdminUnlock.service $wd/livedisk/airootfs/etc/systemd/system/
+cp $wd/windowsAutoAdminUnlock.sh $wd/livedisk/airootfs/root/
 
-mkdir $(dirname $0)/ISO
+mkdir -p $livediskOUT
+mkdir -p $livediskWD
 
-sh $(dirname $0)/livedisk/build.sh -A "Automatic unlocker for Windows Admin account - Arch Livedisk" -L "CHNTPW" -o $(dirname $0)/ISO -w $livediskWD
+sh $wd/livedisk/build.sh -A "Automatic unlocker for Windows Admin account - Arch Livedisk" -L "CHNTPW" -o $livediskOUT -w $livediskWD && logp info "ISO succesfully generated!" || logp warning "ISO failed to generate!"
