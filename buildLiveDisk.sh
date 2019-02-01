bf6f09fe8b0b4184cb10ba6ca8158336dbde5937
PafKlapper <stan@wopwop.cc>
2019-02-01 23:42:19 +0100

    added pacman commands to build tool

--- buildLiveDisk.sh
+++ buildLiveDisk.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # run this script to build livedisk
 
+pacman -Sy archiso arch-install-scripts
+
 cp $(dirname $0)/windowsAutoAdminUnlock.service $(dirname $0)/livedisk/airootfs/etc/systemd/system/
 cp $(dirname $0)/windowsAutoAdminUnlock.sh $(dirname $0)/livedisk/airootfs/root/
 
