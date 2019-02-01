e4c8746d86038f259363eea9d138fe742ff44d3e
PafKlapper <stan@wopwop.cc>
2019-02-01 23:56:01 +0100

    debug

--- buildLiveDisk.sh
+++ buildLiveDisk.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 # run this script to build livedisk
 
+livediskWD=`mktemp -d`
+
+finish {
+rm -rf $livediskWD
+}
+trap finish EXIT SIGINT SIGTERM
+
 pacman -Sy archiso arch-install-scripts
 
 cp $(dirname $0)/windowsAutoAdminUnlock.service $(dirname $0)/livedisk/airootfs/etc/systemd/system/
 cp $(dirname $0)/windowsAutoAdminUnlock.sh $(dirname $0)/livedisk/airootfs/root/
 
-sh $(dirname $0)/livedisk/build.sh -A "Windows Auto Admin Unlocker livedisk "
+mkdir $(dirname $0)/ISO
+
+sh $(dirname $0)/livedisk/build.sh -A "Automatic unlocker for Windows Admin account - Arch Livedisk" -L "CHNTPW" -o $(dirname $0)/ISO -w $livediskWD
