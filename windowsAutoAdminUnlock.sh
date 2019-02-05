961368a903deb79ff69d8e11350170b37ca62ee2
PafKlapper <stan@wopwop.cc>
2019-02-05 10:35:32 +0100

    additional exes can now be sideloaded, powershell commands not yet working

--- windowsAutoAdminUnlock.sh
+++ windowsAutoAdminUnlock.sh
@@ -3,15 +3,15 @@
 # WARNING: this script only supports systems running a SINGLE windows installation, more than one ntfs partition of whatever other kind is no problem.
 
 #GLOBVARS
-finalizeTimeout=5 # set finalizeTimeout to 0 to immediately reboot after script has ran 
+finalizeTimeout=15 # set finalizeTimeout to 0 to immediately reboot after script has ran 
 finalizeAction="poweroff" # set to arbitary string that will be passed to 'eval' and run as last command.
-sideLoadTarget="/sideload" #  arbitrary folder on C:\ which will contain sideLoad files, make sure to prepend a forward slash!
-
+sideLoadTarget="sideload" #  arbitrary folder on C:\ which will contain sideLoad files
+sideLoadExecutables=("HPQFlash/HpqFlash.exe -s -a" "powerSHELL cmd /c pause | out-null") # array of execubles/commands that will be autorun once. Use spaces as seperator. Prepend a powershell command with "powerSHELL"
+# EXAMPLE: sideLoadExecutables=("HPQFlash/HpqFlash.exe -s -a" "powerSHELL whoami")  
 
 #INTVARS
 ntfsBlk=""
 mountPoint="/mnt"
-sideLoadTarget="$mountPoint$sideLoadTarget"
 
 # bash run options
 set -o pipefail
@@ -66,7 +66,8 @@ blkid|grep ntfs| while read ntfsLine; do
 			umount -f $ntfsBlk
 			ntfsfix -d $ntfsBlk
 			if  [ ! $? -eq 0 ]; then
-				logp fatal "Couldn't fix the NTFS filesystem on harddisk $ntfsBlk!"
+				logp warning "Couldn't fix the NTFS filesystem on harddisk $ntfsBlk!"
+				return 1
 			fi
 		else
 			if [ -d $mountPoint/Windows/System32 ]; then
@@ -77,7 +78,8 @@ blkid|grep ntfs| while read ntfsLine; do
 			fi
 		fi
 	else
-		logp fatal "Scripterror in blockdetection: variable \$ntfsblk has invalid value $ntfsBlk "
+		logp warning "Scripterror in blockdetection: variable \$ntfsblk has invalid value $ntfsBlk "
+		return 1
 	fi
 done
 }
@@ -113,7 +115,8 @@ PowerShell.exe -NoProfile -Command "&{ start-process powershell -ArgumentList '-
 EOF
 
 if [ $? -gt 0 ]; then
-	logp fatal "Couldn't insert script to Windows partition @ $mountPoint/autoDisableAdmin.bat! "
+	logp warning "Couldn't insert script to Windows partition @ $mountPoint/autoDisableAdmin.bat! "
+	return 1
 fi
 
 # edit registry to run the cleanup script
@@ -133,6 +136,54 @@ if [ $? -eq 2 ]; then
 else
 	sync && return 1;
 fi
+}
+
+
+addAdditionalSideLoadExecutables() {
+
+for exe in "${sideLoadExecutables[$@]}"; do
+	sanitizedExe="$(echo $exe |sed 's/[^a-zA-Z0-9]//g')"
+
+	echo sanitzedExe: \"$sanitizedExe\"
+	echo Exe: \"$exe\"
+	read
+
+	if ! [ "$(echo $exe | grep -e "^powerSHELL")" = "" ]; then
+		exe="$(echo $exe | sed  -r 's/^powerSHELL//g')"
+		logp info "CMD: \'$exe\' will be run once at next boot!"
+cat>$mountPoint/$sanitizedExe.bat<<EOF
+@ECHO OFF
+PowerShell.exe -NoProfile -Command "&{ start-process powershell -ArgumentList '-noprofile -command "\$( $exe ) -and \$(Remove-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "$sanitizedExe")"' -verb RunAs}" && (goto) 2>nul & del "%~f0"
+EOF
+	else
+		logp info "EXE: 'C:\\$sideLoadTarget\\$exe' will be run once at next boot!"
+cat>$mountPoint/$sanitizedExe.bat<<EOF
+@ECHO OFF
+PowerShell.exe -NoProfile -Command "&{ start-process powershell -ArgumentList \'-noprofile -command "\$(& C:\\$sideLoadTarget\\$exe) -and \$(Remove-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "$sanitizedExe")"\' -verb RunAs}" && (goto) 2>nul & del "%~f0"
+EOF
+	fi
+
+	if ! [ $? -eq 0 ]; then
+		logp warning "Failed to copy powershell script that starts additional sideload executable \'$exe\'..."
+		return 1
+	fi
+	
+( chntpw -e $mountPoint/Windows/System32/config/SOFTWARE<<EOF 
+cd Microsoft\Windows\CurrentVersion\Run
+nv 1 $sanitizedExe
+ed $sanitizedExe
+C:\\$sanitizedExe.bat
+q
+y
+
+EOF
+) 1>/dev/null 2>/dev/null
+
+	if ! [ $? -eq 2 ]; then
+		logp warning "Chntpw magic failed to set \'$exe\' to autolaunch via 'CurrentVersion\Run'!"
+		return 1
+	fi
+done
 }
 
 main()
@@ -171,19 +222,29 @@ echo -e "\e[97m"
 		logp fatal "No dice :("
 	fi
 	
-	logp info "Adding cleanup script to Administrator shell:startup to auto-disable account after first login..."
+	logp info "Adding cleanup script to Administrator to auto-disable account after first login..."
 	if relockAdminUserAfterFirstLogin; then
 		logp info "Succesfully added cleanup script!"
 	else
 		logp fatal "Failed to add cleanup script to Administrator shell:startup to auto-disable account after first login!"
 	fi
 
+
 	if [ -d /root/sideload ]; then
 		logp info "Sideloading files to $sideLoadTarget..."
-		if mkdir -p $sideLoadTarget && cp -av /root/sideload/* $sideLoadTarget && sync; then
+		if mkdir -p $mountPoint/$sideLoadTarget && cp -av /root/sideload/* $mountPoint/$sideLoadTarget && sync; then
 			logp info "Files succesfully sideloaded!"
 		else
 			logp warning "Sideload failed to be copied over!"
+		fi
+	fi
+
+	if [ ${#sideLoadExecutables[@]} -gt 0 ]; then
+		logp info "Adding additional executable to be autorun at next boot..."
+		if addAdditionalSideLoadExecutables; then
+			logp info "Additional executables succesfully set up!"
+		else
+			logp fatal "Additional executables failed to be set up!"
 		fi
 	fi
 
